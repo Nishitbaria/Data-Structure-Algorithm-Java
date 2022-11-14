fe2b68fcc46505c15d5e09d1dc68fbcb06cbe1c0
Nishitbaria <Nishitbaria@gmail.com>
2022-11-14 11:57:49 +0530

    consturucter

---
+++ OOPS/constructer.java
@@ -0,0 +1,12 @@
+
+public class constructer {
+    public static void main(String[] args) {
+        student s1 = new student();
+    }
+}
+class student{
+    
+    student(){
+        System.out.println("constructer is called");
+    }
+    }
