ec56f4d495e83ac39c2f268d1910f8ff52855be5
Nishitbaria <Nishitbaria@gmail.com>
2022-11-27 06:03:12 +0530

    Link-List-adding

--- OOPS/constructer.java
+++ OOPS/constructer.java
@@ -2,11 +2,28 @@
 public class constructer {
     public static void main(String[] args) {
         student s1 = new student();
+        student s2 = new student("Hello");
+        student s3 = new student("Hello");
+
+
     }
 }
 class student{
+  int rollno;
+  String name;
+
     
+
     student(){
         System.out.println("constructer is called");
     }
+student(String name){
+this.name = name;
+}
+
+student(int rollno){
+    this.rollno = rollno;
+}
+
+
     }
